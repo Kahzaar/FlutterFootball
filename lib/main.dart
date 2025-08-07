fd4a7b3f265ae42f41428f798de4f698dc6bb8ba
Andreas Sällström <jakasio_91@hotmail.com>
2025-08-07 22:05:27 +0200

    Sprite loaded

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:footballflameflutter/game/game.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,11 +12,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: GameWidget.controlled(gameFactory: FootballGame.new)
     );
   }
 }
